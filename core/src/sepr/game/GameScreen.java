6a4d4a5094d0712d01407bc43b1ea7f596546fae
PeterBeckk <pb879@york.ac.uk>
2017-11-23 22:14:48 +0000

    map rendering

--- core/src/sepr/game/GameScreen.java
+++ core/src/sepr/game/GameScreen.java
@@ -2,19 +2,22 @@ package sepr.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+
 import java.util.HashMap;
 
 /**
  * Created by Dom's Surface Mark 2 on 16/11/2017.
  */
-public class GameScreen implements Screen{
+public class GameScreen implements Screen, InputProcessor{
     private Main main;
     private Map map;
     private HashMap<Integer, Player> players; // player id mapping to the relevant player
@@ -29,6 +32,8 @@ public class GameScreen implements Screen{
     private SpriteBatch batch;
     private OrthographicCamera camera;
     private Viewport viewport;
+
+    Color test = new Color(0,0,0,0);
 
     /**
      * Performs the game's initial setup
@@ -48,11 +53,14 @@ public class GameScreen implements Screen{
         this.turnOrder = this.players.keySet().toArray(new Integer[0]);
         this.currentPlayer = 0;
 
+        Gdx.input.setInputProcessor(this);
 
         this.wheel = Player.genAttackWheelTexture(30, 2, 1);
 
         this.batch = new SpriteBatch();
         this.camera = new OrthographicCamera(1920, 1080);
+        camera.position.x = 1920 / 2;
+        camera.position.y = 1080 / 2;
         this.viewport = new ScreenViewport(this.camera);
 
         allocateSectors();
@@ -110,23 +118,24 @@ public class GameScreen implements Screen{
     @Override
     public void render(float delta) {
         //all game content to be drawn here
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            camera.translate(-Gdx.input.getDeltaX() * camera.zoom, Gdx.input.getDeltaY() * camera.zoom, 0);
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            camera.translate(-4,0,0);
         }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.P)) {
-            camera.zoom -= 0.01;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            camera.translate(4,0,0);
         }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.O)) {
-            camera.zoom += 0.01;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            camera.translate(0,4,0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            camera.translate(0,-4,0);
         }
 
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         map.render(batch);
-        batch.draw(wheel, 400, 400);
+        //batch.draw(wheel, 0, 0);
         batch.end();
     }
 
@@ -155,5 +164,191 @@ public class GameScreen implements Screen{
     @Override
     public void dispose() {
 
+    }
+    public boolean keyPressed;
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        int hesEast1Pixel = map.hesEastPix1.getPixel(screenX, screenY);
+        int hesEast2Pixel = map.hesEastPix2.getPixel(screenX, screenY);
+        int hesEast3Pixel = map.hesEastPix3.getPixel(screenX, screenY);
+        int hesEast4Pixel = map.hesEastPix4.getPixel(screenX, screenY);
+        int halifax1Pixel = map.halifaxPix1.getPixel(screenX, screenY);
+        int halifax2Pixel = map.halifaxPix2.getPixel(screenX, screenY);
+        int halifax3Pixel = map.halifaxPix3.getPixel(screenX, screenY);
+        int halifax4Pixel = map.halifaxPix4.getPixel(screenX, screenY);
+        int derwent1Pixel = map.derwentPix1.getPixel(screenX, screenY);
+        int derwent2Pixel = map.derwentPix2.getPixel(screenX, screenY);
+        int derwent3Pixel = map.derwentPix3.getPixel(screenX, screenY);
+        int derwent4Pixel = map.derwentPix4.getPixel(screenX, screenY);
+        int alcuin1Pixel = map.alcuinPix1.getPixel(screenX, screenY);
+        int alcuin2Pixel = map.alcuinPix2.getPixel(screenX, screenY);
+        int alcuin3Pixel = map.alcuinPix3.getPixel(screenX, screenY);
+        int vanbrugh1Pixel = map.vanbrughPix1.getPixel(screenX, screenY);
+        int vanbrugh2Pixel = map.vanbrughPix2.getPixel(screenX, screenY);
+        int vanbrugh3Pixel = map.vanbrughPix3.getPixel(screenX, screenY);
+        int vanbrugh4Pixel = map.vanbrughPix4.getPixel(screenX, screenY);
+        int wentworth1Pixel = map.wentworthPix1.getPixel(screenX, screenY);
+        int wentworth2Pixel = map.wentworthPix2.getPixel(screenX, screenY);
+        int james1Pixel = map.jamesPix1.getPixel(screenX, screenY);
+        int james2Pixel = map.jamesPix2.getPixel(screenX, screenY);
+        int james3Pixel = map.jamesPix3.getPixel(screenX, screenY);
+        int james4Pixel = map.jamesPix4.getPixel(screenX, screenY);
+        int neutral1Pixel = map.neutralPix1.getPixel(screenX, screenY);
+        int neutral2Pixel = map.neutralPix2.getPixel(screenX, screenY);
+        int neutral3Pixel = map.neutralPix3.getPixel(screenX, screenY);
+        int neutral4Pixel = map.neutralPix4.getPixel(screenX, screenY);
+        int neutral5Pixel = map.neutralPix5.getPixel(screenX, screenY);
+        int neutral6Pixel = map.neutralPix6.getPixel(screenX, screenY);
+        if(hesEast1Pixel != -256){
+            System.out.println("HIT HES1");
+        } else if(hesEast2Pixel != -256) {
+            System.out.println("HIT HES2");
+        } else if(hesEast3Pixel !=-256) {
+            System.out.println("HIT HES3");
+        } else if(hesEast4Pixel != -256) {
+            System.out.println("HIT HES4");
+        } else if(halifax1Pixel != -256) {
+            System.out.println("HIT HALIFAX1");
+        } else if(halifax2Pixel != -256) {
+            System.out.println("HIT HALIFAX2");
+        } else if(halifax3Pixel != -256) {
+            System.out.println("HIT HALIFAX3");
+        } else if(halifax4Pixel != -256) {
+            System.out.println("HIT HALIFAX4");
+        } else if(derwent1Pixel != -256) {
+            System.out.println("HIT DERWENT1");
+        } else if(derwent2Pixel != -256) {
+            System.out.println("HIT DERWENT2");
+        } else if(derwent3Pixel != -256) {
+            System.out.println("HIT DERWENT3");
+        } else if(derwent4Pixel != -256) {
+            System.out.println("HIT DERWENT4");
+        } else if(alcuin1Pixel != -256) {
+            System.out.println("HIT ALCUIN1");
+        } else if(alcuin2Pixel != -256) {
+            System.out.println("HIT ALCUIN2");
+        } else if(alcuin3Pixel != -256) {
+            System.out.println("HIT ALCUIN3");
+        } else if(vanbrugh1Pixel != -256) {
+            System.out.println("HIT VANBRUGH1");
+        } else if(vanbrugh2Pixel != -256) {
+            System.out.println("HIT VANBRUGH2");
+        } else if(vanbrugh3Pixel != -256) {
+            System.out.println("HIT VANBRUGH3");
+        } else if(vanbrugh4Pixel != -256) {
+            System.out.println("HIT VANBRUGH4");
+        } else if(wentworth1Pixel != -256) {
+            System.out.println("HIT WENTWORTH1");
+        } else if(wentworth2Pixel != -256) {
+            System.out.println("HIT WENTWORTH2");
+        } else if(james1Pixel != -256) {
+            System.out.println("HIT JAMES1");
+        } else if(james2Pixel != -256) {
+            System.out.println("HIT JAMES2");
+        } else if(james3Pixel != -256) {
+            System.out.println("HIT JAMES3");
+        } else if(james4Pixel != -256) {
+                System.out.println("HIT JAMES4");
+        } else if(neutral1Pixel != -256) {
+            System.out.println("HIT NEUTRAL1");
+        } else if(neutral2Pixel != -256) {
+            System.out.println("HIT NEUTRAL2");
+        } else if(neutral3Pixel != -256) {
+            System.out.println("HIT NEUTRAL3");
+        } else if(neutral4Pixel != -256) {
+            System.out.println("HIT NEUTRAL4");
+        } else if(neutral5Pixel != -256) {
+            System.out.println("HIT NEUTRAL5");
+        } else if(neutral6Pixel != -256) {
+            System.out.println("HIT NEUTRAL6");
+        } else {
+            System.out.println("MISS!");
+        }
+
+        for (int x = 0; x < map.hesEastPix1.getWidth(); x++){
+            for (int y = 0; y < map.hesEastPix1.getHeight(); y++){
+                if(map.hesEastPix1.getPixel(x, y) != -256){
+                    Color.rgba8888ToColor(test, map.hesEastPix1.getPixel(x, y));
+                    test.add(0, -255, -255, 0);
+                    map.hesEastPix1.drawPixel(x, y, Color.rgba8888(test));
+                }
+            }
+        }
+        map.hesEast1 = new Texture(map.hesEastPix1);
+
+        for (int x = 0; x < map.hesEastPix2.getWidth(); x++){
+            for (int y = 0; y < map.hesEastPix2.getHeight(); y++){
+                if(map.hesEastPix2.getPixel(x, y) != -256){
+                    Color.rgba8888ToColor(test, map.hesEastPix2.getPixel(x, y));
+                    test.add(0, -255, -255, 0);
+                    map.hesEastPix2.drawPixel(x, y, Color.rgba8888(test));
+                }
+            }
+        }
+        map.hesEast2 = new Texture(map.hesEastPix2);
+
+        for (int x = 0; x < map.hesEastPix3.getWidth(); x++){
+            for (int y = 0; y < map.hesEastPix3.getHeight(); y++){
+                if(map.hesEastPix3.getPixel(x, y) != -256){
+                    Color.rgba8888ToColor(test, map.hesEastPix3.getPixel(x, y));
+                    test.add(0, -255, -255, 0);
+                    map.hesEastPix3.drawPixel(x, y, Color.rgba8888(test));
+                }
+            }
+        }
+        map.hesEast3 = new Texture(map.hesEastPix3);
+
+        for (int x = 0; x < map.hesEastPix4.getWidth(); x++){
+            for (int y = 0; y < map.hesEastPix4.getHeight(); y++){
+                if(map.hesEastPix4.getPixel(x, y) != -256){
+                    Color.rgba8888ToColor(test, map.hesEastPix4.getPixel(x, y));
+                    test.add(0, -255, -255, 0);
+                    map.hesEastPix4.drawPixel(x, y, Color.rgba8888(test));
+                }
+            }
+        }
+        map.hesEast4 = new Texture(map.hesEastPix4);
+
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        if (amount == 1){
+            camera.zoom += 0.1;
+        } else {
+            camera.zoom -= 0.1;
+        }
+        return true;
     }
 }
--- core/src/sepr/game/Map.java
+++ core/src/sepr/game/Map.java
@@ -1,5 +1,8 @@
 package sepr.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -10,11 +13,100 @@ import java.util.HashMap;
  */
 public class Map {
     private HashMap<Integer, Sector> sectors; // mapping of sector ID to the sector object
-    private Texture map;
+
+    public Texture hesEast1, hesEast2, hesEast3, hesEast4;
+    public Texture halifax1, halifax2, halifax3, halifax4;
+    public Texture derwent1, derwent2, derwent3, derwent4;
+    public Texture alcuin1, alcuin2, alcuin3;
+    public Texture vanbrugh1, vanbrugh2, vanbrugh3, vanbrugh4;
+    public Texture wentworth1, wentworth2;
+    public Texture james1, james2, james3, james4;
+    public Texture neutral1, neutral2, neutral3, neutral4, neutral5, neutral6;
+
+    public Pixmap hesEastPix1, hesEastPix2, hesEastPix3, hesEastPix4;
+    public Pixmap halifaxPix1, halifaxPix2, halifaxPix3, halifaxPix4;
+    public Pixmap derwentPix1, derwentPix2, derwentPix3, derwentPix4;
+    public Pixmap alcuinPix1, alcuinPix2, alcuinPix3;
+    public Pixmap vanbrughPix1, vanbrughPix2, vanbrughPix3, vanbrughPix4;
+    public Pixmap wentworthPix1, wentworthPix2;
+    public Pixmap jamesPix1, jamesPix2, jamesPix3, jamesPix4;
+    public Pixmap neutralPix1, neutralPix2, neutralPix3, neutralPix4, neutralPix5, neutralPix6;
+
+    Color test = new Color(0,0,0,0);
 
     public Map() {
         this.sectors = new HashMap<Integer, Sector>();
-        this.map = new Texture("map.jpg");
+
+        this.hesEastPix1 = new Pixmap(Gdx.files.internal("hesEast1.png"));
+        this.hesEastPix2 = new Pixmap(Gdx.files.internal("hesEast2.png"));
+        this.hesEastPix3 = new Pixmap(Gdx.files.internal("hesEast3.png"));
+        this.hesEastPix4 = new Pixmap(Gdx.files.internal("hesEast4.png"));
+
+        this.hesEast1 = new Texture(hesEastPix1);
+        this.hesEast2 = new Texture(hesEastPix2);
+        this.hesEast3 = new Texture(hesEastPix3);
+        this.hesEast4 = new Texture(hesEastPix4);
+
+        this.halifaxPix1 = new Pixmap(Gdx.files.internal("halifax1.png"));
+        this.halifaxPix2 = new Pixmap(Gdx.files.internal("halifax2.png"));
+        this.halifaxPix3 = new Pixmap(Gdx.files.internal("halifax3.png"));
+        this.halifaxPix4 = new Pixmap(Gdx.files.internal("halifax4.png"));
+        this.halifax1 = new Texture(halifaxPix1);
+        this.halifax2 = new Texture(halifaxPix2);
+        this.halifax3 = new Texture(halifaxPix3);
+        this.halifax4 = new Texture(halifaxPix4);
+
+        this.derwentPix1 = new Pixmap(Gdx.files.internal("derwent1.png"));
+        this.derwentPix2 = new Pixmap(Gdx.files.internal("derwent2.png"));
+        this.derwentPix3 = new Pixmap(Gdx.files.internal("derwent3.png"));
+        this.derwentPix4 = new Pixmap(Gdx.files.internal("derwent4.png"));
+        this.derwent1 = new Texture(derwentPix1);
+        this.derwent2 = new Texture(derwentPix2);
+        this.derwent3 = new Texture(derwentPix3);
+        this.derwent4 = new Texture(derwentPix4);
+
+        this.alcuinPix1 = new Pixmap(Gdx.files.internal("alcuin1.png"));
+        this.alcuinPix2 = new Pixmap(Gdx.files.internal("alcuin2.png"));
+        this.alcuinPix3 = new Pixmap(Gdx.files.internal("alcuin3.png"));
+        this.alcuin1 = new Texture(alcuinPix1);
+        this.alcuin2 = new Texture(alcuinPix2);
+        this.alcuin3 = new Texture(alcuinPix3);
+
+        this.vanbrughPix1 = new Pixmap(Gdx.files.internal("vanbrugh1.png"));
+        this.vanbrughPix2 = new Pixmap(Gdx.files.internal("vanbrugh2.png"));
+        this.vanbrughPix3 = new Pixmap(Gdx.files.internal("vanbrugh3.png"));
+        this.vanbrughPix4 = new Pixmap(Gdx.files.internal("vanbrugh4.png"));
+        this.vanbrugh1 = new Texture(vanbrughPix1);
+        this.vanbrugh2 = new Texture(vanbrughPix2);
+        this.vanbrugh3 = new Texture(vanbrughPix3);
+        this.vanbrugh4 = new Texture(vanbrughPix4);
+
+        this.wentworthPix1 = new Pixmap(Gdx.files.internal("wentworth1.png"));
+        this.wentworthPix2 = new Pixmap(Gdx.files.internal("wentworth2.png"));
+        this.wentworth1 = new Texture(wentworthPix1);
+        this.wentworth2 = new Texture(wentworthPix2);
+
+        this.jamesPix1 = new Pixmap(Gdx.files.internal("james1.png"));
+        this.jamesPix2 = new Pixmap(Gdx.files.internal("james2.png"));
+        this.jamesPix3 = new Pixmap(Gdx.files.internal("james3.png"));
+        this.jamesPix4 = new Pixmap(Gdx.files.internal("james4.png"));
+        this.james1 = new Texture(jamesPix1);
+        this.james2 = new Texture(jamesPix2);
+        this.james3 = new Texture(jamesPix3);
+        this.james4 = new Texture(jamesPix4);
+
+        this.neutralPix1 = new Pixmap(Gdx.files.internal("neutral1.png"));
+        this.neutralPix2 = new Pixmap(Gdx.files.internal("neutral2.png"));
+        this.neutralPix3 = new Pixmap(Gdx.files.internal("neutral3.png"));
+        this.neutralPix4 = new Pixmap(Gdx.files.internal("neutral4.png"));
+        this.neutralPix5 = new Pixmap(Gdx.files.internal("neutral5.png"));
+        this.neutralPix6 = new Pixmap(Gdx.files.internal("neutral6.png"));
+        this.neutral1 = new Texture(neutralPix1);
+        this.neutral2 = new Texture(neutralPix2);
+        this.neutral3 = new Texture(neutralPix3);
+        this.neutral4 = new Texture(neutralPix4);
+        this.neutral5 = new Texture(neutralPix5);
+        this.neutral6 = new Texture(neutralPix6);
     }
 
     /**
@@ -49,7 +141,46 @@ public class Map {
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(map, 0, 0);
+        batch.draw(hesEast1, 0, 0);
+        batch.draw(hesEast2, 0, 0);
+        batch.draw(hesEast3, 0, 0);
+        batch.draw(hesEast4, 0, 0);
+
+        batch.draw(halifax1, 0, 0);
+        batch.draw(halifax2, 0, 0);
+        batch.draw(halifax3, 0, 0);
+        batch.draw(halifax4, 0, 0);
+
+        batch.draw(derwent1, 0, 0);
+        batch.draw(derwent2, 0, 0);
+        batch.draw(derwent3, 0, 0);
+        batch.draw(derwent4, 0, 0);
+
+        batch.draw(alcuin1, 0, 0);
+        batch.draw(alcuin2, 0, 0);
+        batch.draw(alcuin3, 0, 0);
+
+        batch.draw(vanbrugh1, 0, 0);
+        batch.draw(vanbrugh2, 0, 0);
+        batch.draw(vanbrugh3, 0, 0);
+        batch.draw(vanbrugh4, 0, 0);
+
+        batch.draw(wentworth1, 0, 0);
+        batch.draw(wentworth2, 0, 0);
+
+        batch.draw(james1, 0, 0);
+        batch.draw(james2, 0, 0);
+        batch.draw(james3, 0, 0);
+        batch.draw(james4, 0, 0);
+
+        batch.draw(neutral1, 0, 0);
+        batch.draw(neutral2, 0, 0);
+        batch.draw(neutral3, 0, 0);
+        batch.draw(neutral4, 0, 0);
+        batch.draw(neutral5, 0, 0);
+        batch.draw(neutral6, 0, 0);
+
+
     }
 
 }
