4479813a145f210fa2e45b6bc035ee7104544213
DomGeoT <dgt505@york.ac.uk>
2018-01-22 07:40:02 +0000

    Final Build

--- desktop/src/sepr/game/desktop/DesktopLauncher.java
+++ desktop/src/sepr/game/desktop/DesktopLauncher.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import sepr.game.Main;
 
+/**
+ * executable http://www.riskydevelopments.co.uk/bod/BoD.zip
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
