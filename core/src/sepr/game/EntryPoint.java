7ac798dc0dbdf730abf489eb87cccafe62a77cba
sdp518 <[sdp518@york.ac.uk]>
2018-02-03 23:00:23 +0000

    Adding enum EntryPoint to reference whether a screen was entered from MenuScreen or Game Screen

---
+++ core/src/sepr/game/EntryPoint.java
@@ -0,0 +1,29 @@
+package sepr.game;
+
+public enum EntryPoint {
+    GAME_SCREEN("GAME SCREEN"),
+    MENU_SCREEN("HUMAN PLAYER");
+
+    private final String shortCode;
+
+    EntryPoint(String code){
+        this.shortCode = code;
+    }
+
+    public String getEntryPoint(){
+        return this.shortCode;
+    }
+
+    /**
+     * converts the string representation of the enum to the enum value
+     * @throws IllegalArgumentException if the text does not match any of the enum's string values
+     * @param text string representation of the enum
+     * @return the enum value of the provided text
+     */
+    public static EntryPoint fromString(String text) throws IllegalArgumentException {
+        for (EntryPoint entryPoint : EntryPoint.values()) {
+            if (entryPoint.getEntryPoint().equals(text)) return entryPoint;
+        }
+        throw new IllegalArgumentException("Text parameter must match one of the enums");
+    }
+}
