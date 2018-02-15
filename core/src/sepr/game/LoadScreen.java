8a70765e6167d83dab1cfa5e432ae3a8ff41cb20
sdp518 <[sdp518@york.ac.uk]>
2018-02-15 20:27:08 +0000

    Working on save/load table implementation

--- core/src/sepr/game/LoadScreen.java
+++ core/src/sepr/game/LoadScreen.java
@@ -7,13 +7,13 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -78,44 +78,74 @@ public class LoadScreen implements Screen{
     // TODO Implement setupSelectSaveTable()
     private Table setupSelectSaveTable() {
         Table saveTable = new Table();
-        saveTable.setDebug(true);
+        saveTable.setDebug(false);
 
-        Table saveOne, saveTwo, saveThree, saveFour;
-        saveOne = new Table();
-        saveTwo = new Table();
-        saveThree = new Table();
-        saveFour = new Table();
-        Table[] saveTables = new Table[] {saveOne, saveTwo, saveThree, saveFour};
+        Label.LabelStyle smallStyle = new Label.LabelStyle();
+        smallStyle.font = WidgetFactory.getFontBig();
+
+        Label.LabelStyle bigStyle = new Label.LabelStyle();
+        bigStyle.font = WidgetFactory.getFontSmall();
+
+        Table[] saveTables = new Table[] {new Table(), new Table(), new Table(), new Table()};
 
         for (final Table t : saveTables) {
             t.setDebug(false);
+            t.setTouchable(Touchable.enabled);
             t.setBackground(new TextureRegionDrawable(new TextureRegion(selectSaveBox, 0,0, 1240, 208)));
             t.addListener(new ClickListener() {
+
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
+                    // IDEK
+                }
+
+                @Override
+                public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+//                    super.enter(event, x, y, pointer, fromActor);
                     t.setBackground(new TextureRegionDrawable(new TextureRegion(selectSaveBox, 0,208, 1240, 208)));
+                }
+
+                @Override
+                public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+//                    super.exit(event, x, y, pointer, toActor);
+                    t.setBackground(new TextureRegionDrawable(new TextureRegion(selectSaveBox, 0,0, 1240, 208)));
                 }
             });
 
+            // TODO Matt save data needs to be read into the below
+
+            // if save in slot
+            //
+
+            t.row().left();
+            t.add(new Image(WidgetFactory.genCollegeLogoDrawable(GameSetupScreen.CollegeName.UNI_OF_YORK))).width(150).height(120).padRight(10).padLeft(10);
+            t.add(new Label("V", smallStyle));
+            t.add(new Image(WidgetFactory.genCollegeLogoDrawable(GameSetupScreen.CollegeName.HES_EAST))).width(150).height(120).padRight(10).padLeft(10);
+
+            // if three or four players loop once or twice respectively
+//            t.add(new Label("V", smallStyle));
+//            t.add(new Image(WidgetFactory.genCollegeLogoDrawable(GameSetupScreen.CollegeName.HES_EAST))).width(150).height(120).padRight(10).padLeft(10);
+
+            t.row().left();
+            t.add(new Label("Player 1", bigStyle)).center();
+            t.add();
+            t.add(new Label("Player 2", bigStyle)).center();
+
+            // if three or four players loop once or twice respectively
+//            t.add();
+//            t.add(new Label("Player 2", bigStyle)).center();
+
+            // if no save in slot
+            //
+
+//            t.row().center();
+//            t.add(new Label("EMPTY SAVE SLOT", smallStyle));
+
+            stage.addActor(t);
+
+            saveTable.row();
+            saveTable.add(t).height(200).padBottom(20);
         }
-
-
-        /*ImageTextButton.ImageTextButtonStyle style = new ImageTextButton.ImageTextButtonStyle();
-
-        style.imageUp = new TextureRegionDrawable(new TextureRegion(selectSaveBox, 0,0, 1240, 208));
-        style.imageDown = new TextureRegionDrawable(new TextureRegion(selectSaveBox, 0,208, 1240, 208));
-        style.imageOver = new TextureRegionDrawable(new TextureRegion(selectSaveBox, 0,208, 1240, 208));
-
-        style.font = WidgetFactory.getFontSmall();*/
-
-        saveTable.row();
-        saveTable.add(saveOne).height(200).padBottom(20);
-        saveTable.row();
-        saveTable.add(saveTwo).height(200).padBottom(20);
-        saveTable.row();
-        saveTable.add(saveThree).height(200).padBottom(20);
-        saveTable.row();
-        saveTable.add(saveFour).height(200).padBottom(20);
 
         return saveTable;
 
@@ -131,8 +161,14 @@ public class LoadScreen implements Screen{
         // add the menu background
         table.background(new TextureRegionDrawable(new TextureRegion(new Texture("uiComponents/menuBackground.png"))));
 
-        table.center();
-        table.add(WidgetFactory.genMenusTopBar("LOAD GAME")).colspan(2);
+        if (entryPoint == EntryPoint.MENU_SCREEN) {
+            table.center();
+            table.add(WidgetFactory.genMenusTopBar("LOAD GAME")).colspan(2);
+        }
+        else {
+            table.center();
+            table.add(WidgetFactory.genMenusTopBar("SAVE GAME")).colspan(2);
+        }
 
         table.row().padTop(60);
         table.add(setupSelectSaveTable());
@@ -173,10 +209,13 @@ public class LoadScreen implements Screen{
         Table subTable = new Table();
         subTable.setDebug(true);
 
+        if (entryPoint != EntryPoint.MENU_SCREEN) {
+            subTable.row();
+            subTable.add(saveButton).fill().height(60).width(300).padBottom(20);
+        }
+
         subTable.row();
-        subTable.add(saveButton).fill().height(60).width(170).padBottom(20);
-        subTable.row();
-        subTable.add(loadButton).fill().height(60).width(170);
+        subTable.add(loadButton).fill().height(60).width(300);
 
         table.add(subTable).expandX();
 
